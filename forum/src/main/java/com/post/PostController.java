41dc6f90287adc457782d1a958b340ad5e491b92
laura.portillo <laura.portillo@solera.com>
2022-10-10 16:35:19 +0200

    show image

--- forum/src/main/java/com/post/PostController.java
+++ forum/src/main/java/com/post/PostController.java
@@ -1,13 +1,22 @@
 package com.post;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -16,11 +25,12 @@ public class PostController {
 	@Autowired
 	private PostServiceImpl postService;
 
-	@GetMapping("/posts")
-	public String getPosts(Model model) {
-		List<Post> listPosts = postService.getPost();
-		model.addAttribute("listPosts", listPosts);
-		return "posts";
+	@RequestMapping(value = "/posts/threads/{thread_id}/posts/{post_id}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+	public void getImages(@PathVariable("thread_id") Integer threadId, @PathVariable("post_id") Integer postId,
+			HttpServletResponse response) throws IOException {
+		String image = postService.image(postId);
+		ClassPathResource imageFile = new ClassPathResource(image);
+		StreamUtils.copy(imageFile.getInputStream(), response.getOutputStream());
 	}
 
 	@GetMapping("/threads/{thread_id}/posts")
--- forum/src/main/java/com/post/PostServiceImpl.java
+++ forum/src/main/java/com/post/PostServiceImpl.java
@@ -1,11 +1,16 @@
 package com.post;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 
 import com.thread.Thread;
 import com.thread.ThreadRepository;
@@ -27,6 +32,17 @@ public class PostServiceImpl implements PostService {
 		Thread thread = threadRepository.findById(post.getThreadFk()).orElse(null);
 		post.setThread(thread);
 		return postRepository.save(post);
+	}
+	
+	public String image(Integer id) {
+		Post post = postRepository.findById(id).orElseThrow();
+		String url = "src/main/resources/loudspeaker-forum.png";
+		if (post.getCategory() == Category.question) {
+			url = "src/main/resources/qm-forum.png";
+		} else if (post.getCategory() == Category.clarification) {
+			url = "src/main/resources/pencil-forum.png";
+		}
+		return url;
 	}
 
 	public List<Post> getPostbyThreads(Integer id) {
